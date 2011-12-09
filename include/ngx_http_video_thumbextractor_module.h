cc18287bea7e7fe55eedf927490a9e86041b16c7
Wandenberg Peixoto <wandenberg@gmail.com>
2011-12-09 10:43:43 -0200

    adding license notes

--- include/ngx_http_video_thumbextractor_module.h
+++ include/ngx_http_video_thumbextractor_module.h
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2011 Wandenberg Peixoto <wandenberg@gmail.com>
+ *
+ * This file is part of Nginx Video Thumb Extractor Module.
+ *
+ * Nginx Video Thumb Extractor Module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nginx Video Thumb Extractor Module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nginx Video Thumb Extractor Module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * ngx_http_video_thumbextractor_module.h
+ *
+ * Created:  Nov 22, 2011
+ * Author:   Wandenberg Peixoto <wandenberg@gmail.com>
+ *
+ */
 #ifndef NGX_HTTP_VIDEO_THUMBEXTRACTOR_MODULE_H_
 #define NGX_HTTP_VIDEO_THUMBEXTRACTOR_MODULE_H_
 
