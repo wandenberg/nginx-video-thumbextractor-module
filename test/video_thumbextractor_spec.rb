a4bce24c9c579f10a38dd8c97c7d13dcba8a5716
Wandenberg Peixoto <wandenberg@gmail.com>
2012-01-09 10:13:21 -0200

    making test more simple and reliable

--- test/video_thumbextractor_spec.rb
+++ test/video_thumbextractor_spec.rb
@@ -39,7 +39,7 @@ describe "when getting a thumb" do
 
     describe "and manipulating 'second' configuration" do
       let!(:configuration) do
-        @video_second = "$http_x_second"
+        @video_second = "$arg_second$http_x_second"
       end
 
       it "should accept complex values" do
@@ -69,33 +69,14 @@ describe "when getting a thumb" do
       end
 
       it "should retturn different images for two different seconds" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4').get :head => {"x-second" => 2} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-            req.response_header.content_length.should_not eq(0)
-            req.response_header["CONTENT_TYPE"].should eq("image/jpeg")
-
-            image = req.response
-
-            req_1 = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4').get :head => {"x-second" => 4} ,:timeout => 10
-            req_1.callback do
-              req_1.response_header.status.should eq(200)
-              req_1.response_header.content_length.should_not eq(0)
-              req_1.response_header["CONTENT_TYPE"].should eq("image/jpeg")
-
-              req_1.response.should_not eq(image)
-
-              EventMachine.stop
-            end
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2')
+        image_1.should_not eq(image('/test_video.mp4?second=4'))
       end
     end
 
     describe "and manipulating 'width' configuration" do
       let!(:configuration) do
-        @image_width = "$http_x_width"
+        @image_width = "$arg_width$http_x_width"
       end
 
       it "should accept complex values" do
@@ -125,35 +106,19 @@ describe "when getting a thumb" do
       end
 
       it "should return an image with full size if zero value is given" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4?second=2').get :head => {"x-width" => 0} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-
-            req.response.should eq(image('/test_video.mp4?second=2'))
-
-            EventMachine.stop
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2&width=0')
+        image_1.should eq(image('/test_video.mp4?second=2'))
       end
 
       it "should return an image with full size if negative value is given" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4?second=2').get :head => {"x-width" => -15} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-
-            req.response.should eq(image('/test_video.mp4?second=2'))
-
-            EventMachine.stop
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2&width=-15')
+        image_1.should eq(image('/test_video.mp4?second=2'))
       end
     end
 
     describe "and manipulating 'height' configuration" do
       let!(:configuration) do
-        @image_height = "$http_x_height"
+        @image_height = "$arg_height$http_x_height"
       end
 
       it "should accept complex values" do
@@ -183,55 +148,23 @@ describe "when getting a thumb" do
       end
 
       it "should return an image with full size if zero value is given" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4?second=2').get :head => {"x-height" => 0} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-
-            req.response.should eq(image('/test_video.mp4?second=2'))
-
-            EventMachine.stop
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2&height=0')
+        image_1.should eq(image('/test_video.mp4?second=2'))
       end
 
       it "should return an image with full size if negative value is given" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4?second=2').get :head => {"x-height" => -15} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-
-            req.response.should eq(image('/test_video.mp4?second=2'))
-
-            EventMachine.stop
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2&height=-15')
+        image_1.should eq(image('/test_video.mp4?second=2'))
       end
 
       it "should return an image with relative size if only height is given" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4?second=2').get :head => {"x-height" => 360} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-
-            req.response.should eq(image('/test_video.mp4?second=2'))
-
-            EventMachine.stop
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2&width=480&height=270')
+        image_1.should eq(image('/test_video.mp4?second=2&height=270'))
       end
 
       it "should return an image with specified size if height and width are given" do
-        EventMachine.run do
-          req = EventMachine::HttpRequest.new(nginx_address + '/test_video.mp4?second=2' + '&width=200').get :head => {"x-height" => 360} ,:timeout => 10
-          req.callback do
-            req.response_header.status.should eq(200)
-
-            req.response.should_not eq(image('/test_video.mp4?second=2'))
-
-            EventMachine.stop
-          end
-        end
+        image_1 = image('/test_video.mp4?second=2')
+        image_1.should_not eq(image('/test_video.mp4?second=2&width=200&height=360'))
       end
     end
   end
@@ -261,5 +194,6 @@ def image(url)
   url = URI.parse(nginx_address + url)
   the_request = Net::HTTP::Get.new(url.request_uri)
   the_response = Net::HTTP.start(url.host, url.port) { |http| http.request(the_request) }
+  the_response.code.should eq("200")
   the_response.body
 end
